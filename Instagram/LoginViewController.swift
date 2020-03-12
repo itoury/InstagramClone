776abb6148d8bd09139e3b8ba20ede6026c235a7
ryuuya.itou <itouruy@gmail.com>
2020-03-12 20:20:01 +0900

    Firebaseのインストールまで完了

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 伊藤龍哉 on 2020/03/11.
+//  Copyright © 2020 ryuuya.itou. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var mailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    @IBAction func handleLoginButton(_ sender: Any) {
+    }
+    
+    @IBAction func handleCreateAccountButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ Instagram/TabBarControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarControllerViewController.swift
+//  Instagram
+//
+//  Created by 伊藤龍哉 on 2020/03/11.
+//  Copyright © 2020 ryuuya.itou. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
