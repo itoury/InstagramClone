8bd71e53b561184f00018def38ec243a5bf5abbf
ryuuya.itou <itouruy@gmail.com>
2020-03-13 23:35:22 +0900

    ログイン画面まで実装

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -15,9 +17,44 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var displayNameTextField: UITextField!
     
     @IBAction func handleLoginButton(_ sender: Any) {
+        
     }
     
     @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+            if address.isEmpty || password.isEmpty || displayName.isEmpty {
+                print("DEBUG_PRINT:何かが空文字です。")
+                return
+            }
+            SVProgressHUD.show()
+            
+            Auth.auth().createUser(withEmail: address, password: password) { authResult, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました。")
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました。")
+                SVProgressHUD.dismiss()
+                
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            print("DEBUG_PRINT: " + error.localizedDescription)
+                            SVProgressHUD.showError(withStatus: "表示名の設定に失敗しました。")
+                            return
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                        SVProgressHUD.dismiss()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+                
+            }
+        }
     }
     
     override func viewDidLoad() {
